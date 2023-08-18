73cc5c011c84ded903325b05d1a8b67949450df1
marz <marz@bb.wakwak.com>
2023-08-19 08:35:47 +0900

    Update marGLSLLite: Simplify the process by managing shader stages with a vector

--- External/marglCommon/marGLSLLite.h
+++ External/marglCommon/marGLSLLite.h
@@ -1,54 +1,35 @@
-int marCreateGLSLProgram(GLuint *programID,
+#pragma once
+
+#include <vector>
+
+// Global debug flag
+extern bool MAR_GLSL_DEBUG;
+
+// Shader information structs
+struct TShaderInfo {
+  const char* filename;
+  unsigned int shadertype;
+};
+
+// Create GLSL program
+int marCreateGLSLProgram(unsigned int *programID,
                          const char *vs_filename,
-                         const char *fs_filename);
+                         const char *fs_filename,
+                         std::vector<TShaderInfo> shaders_4gt = std::vector<TShaderInfo>());
 
-int marCreateGLSLProgram_withG(GLuint *programID,
-                               const char *vs_filename,
-                               const char *fs_filename,
-                               const char *gs_filename);
-
-int marCreateGLSLProgram_withT(GLuint *programID,
-			       const char *vs_filename,
-			       const char *fs_filename,
-			       const char *hs_filename,
-			       const char *ds_filename);
-
-int marCreateGLSLProgram_withTG(GLuint *programID,
-				const char *vs_filename,
-				const char *fs_filename,
-				const char *hs_filename,
-				const char *ds_filename,
-				const char *gs_filename);
-
-//----------------------------------------------------------------------
-// marCreateGLSLProgramMemory* for Memory(Resource)
-//----------------------------------------------------------------------
-
-int marCreateGLSLProgramMemory(GLuint *programID,
-			       const unsigned char *vs_code,
-			       const unsigned char *fs_code,
-			       const int vs_codesize,
-			       const int fs_codesize);
-
-// int marCreateGLSLProgram_fragment(GLuint *programID, const char *fs_filename); // 仕様では動作不定
-// SPIRV は、marSPIRV.{cpp,h}で
-// int marCreateGLSLProgramSPIRV(GLuint *programID, const char *vs_filename, const char *fs_filename);
-
-//int marLinkProgram(GLuint *programID);
-void print_error_4createshader(unsigned int shadertype);
-
+// Function to set the GLSL directory
 void marSetGLSLDir(const char *dir);
 
-
 /*
-  tessellation
+  テッセレーションシェーダーの略語（Tessellation Shader Abbreviations）:
 
-  tessellation control shader: TCS? glslangValidator では、TESC
-  tessellation evalution shader: TES? glslangValidator では、TESE
+  テッセレーション シェーダには、glslangValidator による TCS/TESE など、さまざまな略語がありますが、
+  DirectX シェーダーの命名規則を採用して簡素化します。
 
-  ややこしいので、
+  While there are various abbreviations for tessellation shaders, such as TCS/TESE by glslangValidator, 
+  we will simplify it by adopting the DirectX shader naming conventions:
+  
 
-  tessellation control shader: hull shader: hs
-  tessellation evalution shader: domain shader: ds
-
-  */
+  Tessellation Control Shader (TESC in glslangValidator) -> hull shader: hs
+  Tessellation Evaluation Shader (TESE in glslangValidator) -> domain shader: ds
+*/
